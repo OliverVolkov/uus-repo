e07662886f21feb5b49be0d186ab0c3fcde6fccb
opilane <opilane@klassb306-21.local>
2023-11-15 11:03:54 +0200

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
+
 const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
-
-  // lisame Bootstrapi osa
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+  build: {
+    outDir: '../dist'
   },
-  //
-
   server: {
-    port: 8080,
-    hot: true
+    port: 8080
   }
 }
